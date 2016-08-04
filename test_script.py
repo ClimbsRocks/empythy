d08d9e61906ce960618b986b9f5c00efd01e92ef
ClimbsRocks <ClimbsBytes@gmail.com>
2016-08-04 15:43:49 -0700

    tests predict functionality

--- test_script.py
+++ test_script.py
@@ -3,3 +3,11 @@ from EmpathyMachines import EmpathyMachines
 nlp_classifer = EmpathyMachines()
 
 nlp_classifer.train(print_analytics_results=True)
+
+
+test_strings = ['i hate this new sugary cereal','i hate this new sugary cereal : (','i love this awesome new machine learning library!','TfidfVectorizer is definitely not contained in scikit-learn\'s stopwords corpus']
+
+for text in test_strings:
+    print(nlp_classifer.predict(text))
+
+print(nlp_classifer.predict(test_strings))
