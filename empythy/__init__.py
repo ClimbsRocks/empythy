588ed9e2d88311d105d2125e273f2a0a31d53228
ClimbsRocks <ClimbsBytes@gmail.com>
2016-08-05 10:24:45 -0700

    reorganizes module to be imported and installed in a more pythonic way

---
+++ empythy/__init__.py
@@ -0,0 +1,2 @@
+# importing here allows us to call `from empythy import EmapthyMachines` when importing this module elsewhere
+from EmpathyMachines import EmpathyMachines
--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.5.2',
+    version='0.5.3',
 
     description='An off-the-rack NLP sentiment classifier- upload your own corpus or use the pre-installed ones',
     long_description=long_description,
@@ -71,7 +71,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(where='src'),
+    packages=['empythy'],
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
