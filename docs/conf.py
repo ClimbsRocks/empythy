d25f337625dfa05bd922e082c75ace88496a7bb1
ClimbsRocks <ClimbsBytes@gmail.com>
2016-08-06 19:31:51 -0700

    switches to standard readthedocs theme- i like this!

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,5 @@
+import sphinx_rtd_theme
+
 # -*- coding: utf-8 -*-
 #
 # empythy documentation build configuration file, created by
@@ -53,6 +55,7 @@ master_doc = 'index'
 project = u'empythy'
 copyright = u'2016, Preston Parry'
 author = u'Preston Parry'
+
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -121,7 +124,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -131,6 +134,7 @@ html_theme = 'alabaster'
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
